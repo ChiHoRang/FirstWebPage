fb1006c7acee99a95f2f5decf38a5de376090d21
ChiHoRang <cksgu4359@gmail.com>
2025-06-28 18:24:41 +0900

    Initial commit: Flask project with images and password page

--- app.py
+++ app.py
@@ -1,10 +1,23 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return "My first website"
+# 내가 원하는 비밀번호 (6자리)
+MY_PASSWORD = "230915"
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        pw = request.form.get('password')
+        if pw == MY_PASSWORD:
+            return redirect(url_for('success'))
+        else:
+            return render_template('index.html', error='비밀번호가 틀렸습니다.')
+    return render_template('index.html')
+
+@app.route('/success')
+def success():
+    return render_template('success.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
